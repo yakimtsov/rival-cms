9075fe0ee71bb067f73379d5ede340c8f2025b66
yakimtsov <yakimtsov2@gmail.com>
2022-10-22 14:02:36 +0300

    v1.0.1

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "firebase/app";
 import {getFirestore} from 'firebase/firestore';
-import { getStorage } from 'firebase/storage';
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
@@ -15,5 +14,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app)
-export const storage = getStorage(app)
 export const auth = getAuth()
